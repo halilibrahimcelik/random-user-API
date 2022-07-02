7de78b8bd015cfa995297f749de96bb9cd2c39a1
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-02 16:04:46 +0300

    fetchin data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import Card from "./components/Card/Card";
 function App() {
-  return <div className="App"></div>;
+  const [initialData, setData] = useState();
+  const url = "https://randomuser.me/api/";
+  const getData = async () => {
+    const { data } = await axios.get(url);
+    console.log(data);
+    setData(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="App">
+      <Card {...initialData} />
+    </div>
+  );
 }
 
 export default App;

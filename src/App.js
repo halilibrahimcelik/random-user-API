a1d425baa21ea50b492babf87bc9cae5b5125b03
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-03 08:38:00 +0300

    Adding refresh logic to the card

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ function App() {
   const [initialData, setData] = useState();
   const url = "https://randomuser.me/api/";
   const getData = async () => {
-    const { data } = await axios.get(url);
-    console.log(data);
-    setData(data);
+    try {
+      const { data } = await axios.get(url);
+
+      console.log(data);
+      setData(data.results[0]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     getData();
   }, []);
-  return (
-    <div className="App">
-      <Card {...initialData} />
-    </div>
-  );
+  return <div className="App">{initialData && <Card {...initialData} />}</div>;
 }
 
 export default App;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
-const Card = () => {
+ import styles from "./card.module.scss"
+const Card = (props) => {
+  console.log("props", props);
+  const {name,location,phone,email,picture,dob,registered}=props;
+  console.log(phone,email,picture.thumbnail,name)
   return (
-    <div>Card</div>
+    <>
+<section>
+
+<div className="card" style={{ width: "18rem" }}>
+  <img className="card-img-top " src={picture.large}  alt={name} />
+  <div className="card-body">
+    <h5 className="card-title">2323 </h5>
+    <p className="card-text">
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </p>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+    <li className="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div className="card-body">
+   
+  </div>
+</div>
+</section>
+    
+    </>
   )
 }
 
